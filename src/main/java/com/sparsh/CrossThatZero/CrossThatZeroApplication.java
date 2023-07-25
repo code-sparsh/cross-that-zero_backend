f2080969432f2680b7c02839b58bcd53a120ea71
Sparsh Sethi <sparshsethi15@gmail.com>
2023-07-25 22:04:16 +0530

    Initialized a new spring project
    Signed-off-by: Sparsh Sethi <sparshsethi15@gmail.com>

---
+++ src/main/java/com/sparsh/CrossThatZero/CrossThatZeroApplication.java
@@ -0,0 +1,13 @@
+package com.sparsh.CrossThatZero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossThatZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossThatZeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparsh/CrossThatZero/CrossThatZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparsh.CrossThatZero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossThatZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
